38e1fc554aeb9e460a3d6e52d99b0d5031c1ec2e
dhanlmao <dhanjeetsingh49@gmail.com>
2025-03-05 19:22:05 +0000

    new

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 window.addEventListener("load", function(){
     const form = this.document.querySelector('#contact form');
     form.addEventListener('submit', sendMessage);
+
+    const reset_success = this.document.querySelector("#reset_success");
+    reset_success.addEventListener("click", reset);
+
+    const reset_error = this.document.querySelector("#reset_error");
+    reset_error.addEventListener("click", reset);
 });
+
+function reset(evt){
+    evt.preventDefault();
+    document.querySelector('#success').style.display = 'none';
+    document.querySelector('#error').style.display = 'none';
+    document.querySelector('#loading').style.display = 'none';
+    document.querySelector('#contact').style.display = 'block';
+
+    if(this.getAttribute('id') == 'reset_success'){
+        document.querySelector('#email').value = '';
+        document.querySelector('#subject').value = '';
+        document.querySelector('#message').value = '';
+    }
+
+}
+
 
 async function sendMessage(evt){
     evt.preventDefault();
@@ -38,4 +60,32 @@ if (message.length == 0){
     fields_ok=false;
 }else{
     hint_message.style.display='none';
+}
+
+if (fields_ok){
+
+    //hide form and show loading icon
+    document.querySelector('#contact').style.display = 'none';
+    document.querySelector('#loading').style.display = 'block';
+
+    //prepare data for transport to server
+    const data = new FormData();
+    data.append('email', email);
+    data.append('subject', subject);
+    data.append('message', message);
+
+    //simulate delay when submitting the data to the server
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    const success = Math.random() > 0.25;
+
+    //hide loading icon when we receive the response
+    document.querySelector("#loading").style.display = "none";
+
+    //show success or error depending on the response
+    if(success){
+        document.querySelector("#success").style.display = "block";
+    }
+    else{
+        document.querySelector('#error').style.display = "block";
+    }
 }
